b6f4efc57fd2a52ae9cf88467c4833b598251bfe
LushoJL <josespinaluque@gmail.com>
2020-04-03 16:27:49 -0400

    septimo video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CursosComponent} from './cursos.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent
+    CursosComponent,
+
   ],
   imports: [
     BrowserModule,
